78519507cabf605ae805e3f80c78032d798421e5
Alex Bonine <lemurdev@gmail.com>
2020-10-11 16:14:06 -0400

    Speed up by using same browser window

--- getPic.js
+++ getPic.js
@@ -7,73 +7,83 @@ const IMG_SELECTOR = '.FFVAD';
 const DATE_SELECTOR = '.Nzb55';
 const NEXT_SELECTOR = '._6CZji';//'.coreSpriteRightChevron'; //'.SWk3c'
 const SAVE_INFO_SELECTOR = '.sqdOP';
+const USERNAME_SELECTOR = 'header a.sqdOP';
+const TITLE_SELECTOR = 'head > title';
 
-const getPic = ({ defaultDir, destPath, linksFile }) => (imgUrl, altName = '', number) => {
-  return puppeteer.launch({ headless: true /*false, devtools: true, slowMo: 250*/ }).then(async (browser) => {
-    const page = await browser.newPage();
+let browser;
+let page;
+
+const getPic = ({ defaultDir, destPath, linksFile }) => async (imgUrl, altName = '', number) => {
+  if (!browser) {
+    browser = await puppeteer.launch({ headless: true /*false, devtools: true, slowMo: 250*/ });
+  }
+
+  if (!page) {
+    page = await browser.newPage();
     page.on('console', console.log);
-    let src, dirName, fileName; 
-    
-    if (imgUrl.match(/.(gif|jpg|jpeg)$/)) {
-      src = imgUrl;
-      dirName = defaultDir;
-      fileName = imgUrl.replace(/(.*)(.(gif|jpg|jpeg))$/, `${altName}$2`).replace('@', '');
-    } else {
-      const openPage = await page.goto(imgUrl, { waitUntil: 'domcontentloaded' });
+  }
 
-      if (openPage.url().includes('login')) {
-        await page.waitForSelector('input[name=username]');
-        const usernameInput = await page.$('input[name=username]');
-        if (usernameInput) {
-          await usernameInput.type('thug_wagon');
-          const passwordInput = await page.$('input[name=password]');
-          await passwordInput.type('SexyTime10');
-          await Promise.all([passwordInput.press('Enter'), page.waitForNavigation()]);
+  let src, dirName, fileName;
 
-          const saveInfoButton = await page.$(SAVE_INFO_SELECTOR);
-          if (saveInfoButton) {
-            await Promise.all([saveInfoButton.click(), page.waitForNavigation()]);
-          }
+  if (imgUrl.match(/.(gif|jpg|jpeg)$/)) {
+    src = imgUrl;
+    dirName = defaultDir;
+    fileName = imgUrl.replace(/(.*)(.(gif|jpg|jpeg))$/, `${altName}$2`).replace('@', '');
+  } else {
+    const openPage = await page.goto(imgUrl, { waitUntil: 'domcontentloaded' });
+
+    if (openPage.url().includes('login')) {
+      await page.waitForSelector('input[name=username]');
+      const usernameInput = await page.$('input[name=username]');
+      if (usernameInput) {
+        await usernameInput.type('thug_wagon');
+        const passwordInput = await page.$('input[name=password]');
+        await passwordInput.type('SexyTime10');
+        await Promise.all([passwordInput.press('Enter'), page.waitForNavigation()]);
+
+        const saveInfoButton = await page.$(SAVE_INFO_SELECTOR);
+        if (saveInfoButton) {
+          await Promise.all([saveInfoButton.click(), page.waitForNavigation()]);
         }
       }
+    }
 
-      if (number > 1) {
-        for (let i = 1; i < +number; i++) {
-          await page.click(NEXT_SELECTOR); // click next
-        }
-      } else { // add number if part of series
-        const exists = await page.$(NEXT_SELECTOR);
-        if (exists) {
-          number = 1;
-        }
+    if (number > 1) {
+      for (let i = 1; i < +number; i++) {
+        await page.click(NEXT_SELECTOR); // click next
       }
-
-      const img = await page.$$eval(IMG_SELECTOR, (elArray, index) => {
-        const el = elArray[index];
-        return { src: el.src, alt: el.alt }; // was removing ? from src .replace(/[^.*]\?.*/, '')
-      }, number ? number - 1 : 0);
-
-      src = img.src;
-      dirName = await page.$eval('header a', el => el.innerHTML);
-      const date = await page.$eval(DATE_SELECTOR, el => el.title);
-      const title = await page.$eval('head > title', el => el.innerHTML.replace(/ ?\n/, ' ').replace(/(.*)“(.*)”/mi, '$2').replace(/\/|@/gi, ''));
-      const imageAlt = ''; // img.alt ? img.alt.replace('@', '') : ''; // now is description from facebook
-      fileName = `${altName || imageAlt || title}${number ? `-${number}` : ''}-${date}.jpg`; //[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+
+    } else { // add number if part of series
+      const exists = await page.$(NEXT_SELECTOR);
+      if (exists) {
+        number = 1;
+      }
     }
 
-    const viewSource = await page.goto(src);
-    const dir = `${destPath}/${dirName}`;
-    fs.ensureDirSync(dir);
-    const filePath = `${dir}/${fileName}`;
+    const img = await page.$$eval(IMG_SELECTOR, (elArray, index) => {
+      const el = elArray[index];
+      return { src: el.src, alt: el.alt }; // was removing ? from src .replace(/[^.*]\?.*/, '')
+    }, number ? number - 1 : 0);
 
-    await writeFile(filePath, await viewSource.buffer());
+    src = img.src;
+    dirName = await page.$eval(USERNAME_SELECTOR, el => el.innerHTML);
+    const date = await page.$eval(DATE_SELECTOR, el => el.title);
+    const title = await page.$eval(TITLE_SELECTOR, el => el.innerHTML.replace(/ ?\n/, ' ').replace(/(.*)“(.*)”/mi, '$2').replace(/\/|@/gi, ''));
+    const imageAlt = ''; // img.alt ? img.alt.replace('@', '') : ''; // now is description from facebook
+    fileName = `${altName || imageAlt || title}${number ? `-${number}` : ''}-${date}.jpg`; //[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+
+  }
 
-    if (linksFile) {
-      await updateLinks(linksFile, { dir: dirName, name: fileName, date: new Date().toISOString(), url: src });
-    }
+  const viewSource = await page.goto(src);
+  const dir = `${destPath}/${dirName}`;
+  fs.ensureDirSync(dir);
+  const filePath = `${dir}/${fileName}`;
 
-    browser.close();
-  });
+  await writeFile(filePath, await viewSource.buffer());
+
+  if (linksFile) {
+    await updateLinks(linksFile, { dir: dirName, name: fileName, date: new Date().toISOString(), url: src });
+  }
+
+  // browser.close();
 }
 
 module.exports = getPic;
